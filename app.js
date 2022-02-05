9596672d1d45be95621f5f8f0e790db0e15f1295
Eboyrr <99083729+Eboyrr@users.noreply.github.com>
2022-02-05 22:53:09 +0700

    Update app.js

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ var port=process.env.PORT
 var app=express()
 
 app.get('/',(req,res)=>{
-    res.sendFile(__dirname+'index.html')
+    res.sendFile(__dirname+'/index.html')
 })
 
 app.listen(port,console.log(port))
